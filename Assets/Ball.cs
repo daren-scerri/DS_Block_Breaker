1bdbf723f9000451c5cb0f6966aec39a576700b6
daren-scerri <daren.scerri@mcast.edu.mt>
2019-10-18 09:33:56 +0200

    added ball launcher

---
+++ Assets/Ball.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    [SerializeField] Paddle mypaddle;
+    [SerializeField] float xVelocity = 2f;
+    [SerializeField] float yVelocity = 15f;
+
+
+    Vector2 paddleToBallDistance;
+
+    bool hasStarted = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        paddleToBallDistance = this.transform.position - mypaddle.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (!hasStarted)
+        {
+            LockBalltoPaddle();
+            LaunchOnMouseClick();
+        }
+
+
+    }
+
+
+    void LockBalltoPaddle()
+    {
+        Vector2 mypaddleposition = new Vector2(mypaddle.transform.position.x, mypaddle.transform.position.y);
+
+        this.transform.position = mypaddleposition + paddleToBallDistance;
+
+    }
+
+
+    private void LaunchOnMouseClick()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            hasStarted = true;
+            this.GetComponent<Rigidbody2D>().velocity = new Vector2(xVelocity, yVelocity);
+        }
+
+    }
+
+
+
+
+}
--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -4,20 +4,31 @@ using UnityEngine;
 
 public class Paddle : MonoBehaviour
 {
-
-    //   GameObject mypaddle;
+    [SerializeField] float ScreenWidthInUnits = 16f;
+    [SerializeField] float minX = 1f;
+    [SerializeField] float maxX = 15f;
     // Start is called before the first frame update
     void Start()
     {
-
     }
+
     // Update is called once per frame
     void Update()
     {
+        
+        float mousePosition = Input.mousePosition.x / Screen.width * ScreenWidthInUnits;
+        Debug.Log(mousePosition);
+        float limitMousePosition = Mathf.Clamp(mousePosition, minX, maxX);
+        Debug.Log(limitMousePosition);
+        //print(Input.mousePosition.x);
+        Vector2 paddlePosition = new Vector2(limitMousePosition, transform.transform.position.y);
+         transform.position = paddlePosition;
+        
 
-
-        float xPoint = Input.mousePosition.x;
-        transform.position = Camera.main.ScreenToWorldPoint(new Vector3(xPoint, 0, 0)) - new Vector3(0,-1,Camera.main.transform.position.z); //+ (0,transform.position.x, transform.position.y);
+        
+        //teacher`s solution
+        //float xPoint = Input.mousePosition.x;
+        //transform.position = Camera.main.ScreenToViewportPoint(new Vector3(xPoint,0,0)) -new Vector3 (0,-1,Camera.main.transform.position.z);
 
     }
 }
