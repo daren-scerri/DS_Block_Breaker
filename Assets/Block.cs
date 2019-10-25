d46ff1537cf3dd24a9870ddffb1d187afc4ae9f4
daren-scerri <daren.scerri@mcast.edu.mt>
2019-10-25 09:20:30 +0200

    Including Ball Spawner challenge

---
+++ Assets/Block.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour
+{
+
+    [SerializeField] int hitCount = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        hitCount--;
+        Debug.Log("Block Destroyed");
+        if (hitCount<=0) Destroy(this.gameObject);
+
+    }
+}
---
+++ Assets/BlockSpawner.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockSpawner : MonoBehaviour
+{
+    [SerializeField] List<Vector3> hit1Blocks = new List<Vector3>();
+    [SerializeField] List<Vector3> hit2Blocks = new List<Vector3>();
+    [SerializeField] GameObject hit1Prefab;
+    [SerializeField] GameObject hit2Prefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+      
+        for (int i=0; i<hit1Blocks.Count; i++)
+        {
+            Instantiate(hit1Prefab, hit1Blocks[i], Quaternion.identity);
+
+        }
+
+
+        for (int i = 0; i < hit2Blocks.Count; i++)
+        {
+            Instantiate(hit2Prefab, hit2Blocks[i], Quaternion.identity);
+
+        }
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
